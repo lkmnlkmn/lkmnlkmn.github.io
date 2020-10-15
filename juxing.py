e9cbc0341095f4ec926a5c0767e202d91451378a
“lkmnlkmn” <2351451891@qq.com>
2020-10-15 14:37:09 +0800

    2

--- juxing.py
+++ juxing.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 def rect_wave(x, c, c0):     
-if x >= (c+c0):
-     r = 0.0
-elif x < c0:
-     r = 0.0
-else :
-     r = 1
-return r
+     if x >= (c+c0):
+          r = 0.0
+     elif x < c0:
+          r = 0.0
+     else :
+          r = 1
+     return r
 
 
 x = np.linspace(-2, 4, 1000)
